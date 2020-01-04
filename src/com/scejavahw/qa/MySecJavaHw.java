b04cfb47e36a03c99d61d44620c39721cb99e8f4
Shabbir  Khan <shabbirkhan@Shabbirs-MacBook-Pro.local>
2020-01-04 17:52:50 -0500

    addingfile

---
+++ src/com/scejavahw/qa/MySecJavaHw.java
@@ -0,0 +1,79 @@
+package com.scejavahw.qa;
+
+public class MySecJavaHw {
+
+
+
+    public static void main (String []agrs){
+
+        //Calling the Method into Main Method
+
+        firstMethod();
+        mySecMethod();
+        myThirdMethod();
+        myFourthMethod();
+        myFifthMethod();
+    }
+
+    public static void firstMethod (){
+
+        int mymarks = 100;
+
+        System.out.println(mymarks);
+
+        String myFirstJavaHw = "I have to study hard to succeed";
+
+        System.out.println(myFirstJavaHw);
+
+
+    }
+
+    public static void mySecMethod(){
+
+        int myLikeNumber = 100;
+
+        String myGoal = "Always to get 100 in all aspects of life";
+
+        System.out.println(myLikeNumber + " " +myGoal);
+
+
+
+    }
+
+    public static void myThirdMethod(){
+
+        int myTargetToWork = 10;
+
+        String work = "I will work in Software Industry";
+
+        String retire = "Enjoy";
+
+        System.out.println(work + " " + myTargetToWork + " " + retire);
+
+
+    }
+
+    public static void myFourthMethod(){
+
+        int myAge = 36;
+
+        String ethics = "Always stand for rights";
+
+        int rightPeople = 10;
+
+        String lessEthics = "Almost 99% of people in this earth";
+
+        System.out.println(myAge + " " + ethics + " " + rightPeople + " " + lessEthics);
+    }
+
+    public static void myFifthMethod(){
+
+        int number = 100;
+
+        String homework = "My goal is to get";
+
+        System.out.println(homework + " " + number);
+    }
+
+
+}
